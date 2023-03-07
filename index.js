762c6b36b90be3b12aa6d17f82b60d53d62c4a5d
Suman-Chandra-Barman <sumandranathbarman@gmail.com>
2023-03-07 20:41:12 +0600

    connect with database and  products api created

---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.uhbaknf.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+const dbConnect = () => {
+  try {
+    client.connect();
+  } catch (error) {
+    console.error(error);
+  }
+};
+dbConnect();
+
+// database and collection create
+const productCollection = client.db("green_plant").collection("products");
+
+// root api
+app.get("/", (req, res) => {
+  try {
+    res.send("Green Plant server is running");
+  } catch (error) {
+    res.send("Server is not working");
+  }
+});
+
+// product api
+app.get("/products", async (req, res) => {
+  console.log("product");
+  try {
+    const query = {};
+    const products = await productCollection.find(query).toArray();
+    res.send(products);
+    console.log(products);
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+app.listen(port, () => {
+  console.log("Server is running on port", port);
+});
